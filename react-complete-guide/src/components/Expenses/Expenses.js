604cb1561d458379bae521d696c25313ab97af5c
Daniel Lavalliere <d.lavalliere@videotron.ca>
2021-05-29 17:42:33 -0400

    Added ne component ExpenseFilter

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
 import "./Expenses.css"
 
 function Expenses( props ) {
+  const [filteredYear, setFilteredYear] = useState('2022');
   return (
-    <Card className="expenses">
-      {
-        props.expenses.map((expense, index) => {
-          return <ExpenseItem key={index} title={expense.title} amount={expense.amount} date={expense.date}/>
-        })
-      }
-    </Card>
+    <div>
+        <Card className="expenses">
+            <ExpensesFilter  selectedYear={filteredYear}
+                             setFilteredYear={setFilteredYear}/>
+            {
+                props.expenses.map((expense, index) => {
+                  return <ExpenseItem key={index}
+                                      title={expense.title}
+                                      amount={expense.amount}
+                                      date={expense.date}/>
+                })
+            }
+        </Card>
+    </div>
   )
 }
 
---
+++ react-complete-guide/src/components/Expenses/ExpensesFilter.js
@@ -0,0 +1,31 @@
+import React from "react"
+import "./ExpensesFilter.css"
+
+const ExpensesFilter = ( props ) => {
+    const selectEventHandler = (event) => {
+        props.setFilteredYear(event.target.value)
+        console.log(event.target.value)
+    }
+    return (
+        <div className="expenses-filter">
+            <div className="expenses-filter__control">
+                <label>Filter by year</label>
+                <select value={props.selectedYear} name="filteredYears"
+                        id="filteredYears"
+                        onChange={selectEventHandler} >
+                    <option value="2019">2019</option>
+                    <option value="2020">2020</option>
+                    <option value="2021">2021</option>
+                    <option value="2022">2022</option>
+                    <option value="2023">2023</option>
+                    <option value="2024">2024</option>
+                    <option value="2025">2025</option>
+                    <option value="2026">2026</option>
+                </select>
+            </div>
+        </div>
+    );
+}
+
+
+export default ExpensesFilter;
